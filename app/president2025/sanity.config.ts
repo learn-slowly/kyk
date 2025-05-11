70610ae06cb0af4aad21286260a4f7a6f22e79bd
pagezero <redoutk@gmail.com>
2025-05-11 11:40:22 +0900

    error fixed

--- app/president2025/sanity.config.ts
+++ app/president2025/sanity.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   projectId: 'qpvtzhxq',
   dataset: 'production',
 
-  plugins: [structure(), visionTool()],
+  plugins: [structure({}), visionTool()],
 
   schema: {
     types: schemaTypes,
