b286117077fab2650a70654c248905c4c85a720d
walkstoplookback <37977515+learn-slowly@users.noreply.github.com>
2025-05-17 23:13:55 +0900

    ...

--- app/president2025/components/MapPositionInput.tsx
+++ app/president2025/components/MapPositionInput.tsx
@@ -253,7 +253,7 @@ const MapPositionInput: React.FC<MapPositionInputProps> = (props) => {
       </Flex>
       
       <Card padding={0} radius={2} shadow={1} overflow="hidden">
-        <Box height="500px">
+        <Box style={{height: '500px'}}>
           {loading ? (
             <Flex align="center" justify="center" height="100%">
               <Text>관계도를 불러오는 중입니다...</Text>
