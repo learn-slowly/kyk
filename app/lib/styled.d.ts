9512205bd42b51d704ffb6f0ee2103bda1366c41
walkstoplookback <37977515+learn-slowly@users.noreply.github.com>
2025-05-18 13:48:22 +0900

    fix: styled-components 테마 인터페이스 명시적 정의로 수정

--- app/lib/styled.d.ts
+++ app/lib/styled.d.ts
@@ -1,6 +1,81 @@
 import 'styled-components';
-import { Theme } from './theme';
+
+// 테마 인터페이스 정의
+interface CustomTheme {
+  colors: {
+    primary: {
+      red: string;
+      yellow: string;
+      green: string;
+    };
+    text: {
+      primary: string;
+      secondary: string;
+      white: string;
+      lightWhite: string;
+    };
+    background: {
+      white: string;
+      black: string;
+      overlay: string;
+      lightOverlay: string;
+    };
+    gradient: string;
+  };
+  fonts: {
+    primary: string;
+    secondary: string;
+  };
+  fontSizes: {
+    xs: string;
+    sm: string;
+    md: string;
+    lg: string;
+    xl: string;
+    '2xl': string;
+    '3xl': string;
+    '4xl': string;
+    '5xl': string;
+  };
+  fontWeights: {
+    light: number;
+    normal: number;
+    medium: number;
+    semibold: number;
+    bold: number;
+  };
+  breakpoints: {
+    sm: string;
+    md: string;
+    lg: string;
+    xl: string;
+  };
+  spacing: {
+    xs: string;
+    sm: string;
+    md: string;
+    lg: string;
+    xl: string;
+  };
+  shadows: {
+    sm: string;
+    md: string;
+    lg: string;
+  };
+  transitions: {
+    fast: string;
+    normal: string;
+    slow: string;
+  };
+  zIndices: {
+    dropdown: number;
+    sticky: number;
+    fixed: number;
+    modal: number;
+    tooltip: number;
+  };
+}
 
 declare module 'styled-components' {
-  export type DefaultTheme = Theme;
+  export interface DefaultTheme extends CustomTheme {}
 } 
